3780fc5d90e5c9f2439c77c0db5432dcd13e27fa
HibbaAbas <hibabbas@gmail.com>
2023-04-13 18:10:25 -0700

    students table created and tested

--- students.sql
+++ students.sql
@@ -6,20 +6,11 @@ CREATE TABLE students
     age NUMERIC(3)
 );
 
-INSERT INTO students
-VALUES (1, 'Fred', 'Flintstone', 35);
+INSERT INTO students(id, firstname, lastname, age)
+VALUES (1, 'Fred', 'Flintstone', 35),
+        (2, 'Wilma', 'Flintstone', 33),
+        (3, 'Barney', 'Rubble', 33),
+        (4, 'Betty', 'Rubble', 29),
+        (5, 'Pebbles', 'Flintstone', 1),
+        (6, 'Bam-Bam', 'Rubble', 1);
 
-INSERT INTO students
-VALUES (2, 'Wilma, 'Flintstone', 29);
-
-INSERT INTO students
-VALUES (3, 'Barney', 'Rubble', 33);
-
-INSERT INTO students
-VALUES (4, 'Betty', 'Rubble', 29);
-
-INSERT INTO students 
-VALUES (5, 'Pebbles', 'Flintstone', 1);
-
-INSERT INTO students
-VALUES (6, 'Bam-Bam', 'Rubble', 1);
