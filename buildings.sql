a11ec4ba2981a85fe5aae92fbe629d2be93c1540
HibbaAbas <hibabbas@gmail.com>
2023-04-14 15:12:57 -0700

    buildings table completed and tested

--- buildings.sql
+++ buildings.sql
@@ -1,11 +1,24 @@
 CREATE TABLE buildings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(80) NOT NULL,
-    shortname VARCHAR(10) UNIQUE, 
-    id INTEGER(RAND()) PRIMARY KEY
+    shortname VARCHAR(10) UNIQUE
 );
 
 CREATE TABLE rooms (
     number INTEGER,
-    buildingid , 
+    buildingid INTEGER REFERENCES buildings(id), 
     seating INTEGER
 );
+
+INSERT INTO buildings (name, shortname)
+VALUES 
+    ('Odegardd', 'Ode'),
+    ('Mary Gates Hall', 'MGH');
+
+
+INSERT INTO rooms (number, buildingid, seating)
+VALUES 
+    (112, (SELECT id FROM buildings WHERE name = 'Odegardd'), 200),
+    (200, (SELECT id FROM buildings WHERE name = 'Odegardd'), 40),
+    (314, (SELECT id FROM buildings WHERE name = 'Mary Gates Hall'), 30);
+
